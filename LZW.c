d7b80cf96ec073f044a0ac482e8ed0bb915e0036
Owen Jaques <owenjaques@uvic.ca>
2019-10-16 16:12:33 -0700

    intitialized dic

--- LZW.c
+++ LZW.c
@@ -16,6 +16,36 @@ unsigned char dict[DICTSIZE][ENTRYSIZE];  /* of 30 chars max; the first byte */
                                           /* will be reserved for padding    */
                                           /* the last byte (if necessary)    */
 
+void initDic(){
+	int i;
+	for(i = 0; i < 256; i++){
+		dict[i][0] = 1;
+		dict[i][1] = i;
+	}
+}
+
+/*****************************************************************************/
+/* encode() performs the Lempel Ziv Welch compression from the algorithm in  */
+/* the assignment specification. The strings in the dictionary have to be    */
+/* handled carefully since 0 may be a valid character in a string (we can't  */
+/* use the standard C string handling functions, since they will interpret   */
+/* the 0 as the end of string marker). Again, writing the codes is handled   */
+/* by a separate function, just so I don't have to worry about writing 12    */
+/* bit numbers inside this algorithm.                                        */
+void encode(FILE *in, FILE *out) {
+	char w = 0;
+	char k;
+	for(k = fgetc(in); k != EOF; k = fgetc(in)){
+	}
+}
+
+/*****************************************************************************/
+/* decode() performs the Lempel Ziv Welch decompression from the algorithm   */
+/* in the assignment specification.                                          */
+void decode(FILE *in, FILE *out) {
+	// TODO implement
+}
+
 
 int main(int argc, char *argv[]){
 	if(argc <= 2 || argv[2][0] != 'e' && argv[2][0] != 'd'){
@@ -37,15 +67,15 @@ int main(int argc, char *argv[]){
 	
 	int state = argv[2][0] == 'e' ? ENCODE : DECODE;
 	FILE* output_file;
+	char file_name[256];
+	initDic();
 	switch(state){
 		case ENCODE:
-			printf("case is encode\n");
-			char file_name[50];
-			snprintf(file_name, sizeof(argv[1]) + 3*sizeof(char), "%s.LZW", argv[1]);
+			snprintf(file_name, 256*sizeof(char), "%s.LZW", argv[1]);
 			output_file = fopen(file_name, "w");
+			encode(input_file, output_file);
 			break;
 		case DECODE:
-			printf("case is decode\n");
 			break;
 	}
 
