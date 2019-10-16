d581fa5a13f80b9deca072c54ab6fae8d4e93fcf
Owen Jaques <owenjaques@uvic.ca>
2019-10-16 15:07:38 -0700

    creates .LZW file for encode

--- LZW.c
+++ LZW.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define TRUE 1
+#define FALSE 0
+
+#define DICTSIZE 4096                     /* allow 4096 entries in the dict  */
+#define ENTRYSIZE 32
+
+#define ENCODE 1
+#define DECODE 2
+
+unsigned char dict[DICTSIZE][ENTRYSIZE];  /* of 30 chars max; the first byte */
+                                          /* is string length; index 0xFFF   */
+                                          /* will be reserved for padding    */
+                                          /* the last byte (if necessary)    */
+
+
 int main(int argc, char *argv[]){
 	if(argc <= 2 || argv[2][0] != 'e' && argv[2][0] != 'd'){
 		printf("Invalid Usage, expected: LZW {input_file} [e|d]\n");
@@ -19,10 +34,20 @@ int main(int argc, char *argv[]){
 		printf("Read error: file not found or cannot be read\n");
 		exit(2);
 	}
-
-	FILE* output_file = fopen(NAME_OF_FILE, "w");
-
-
+	
+	int state = argv[2][0] == 'e' ? ENCODE : DECODE;
+	FILE* output_file;
+	switch(state){
+		case ENCODE:
+			printf("case is encode\n");
+			char file_name[50];
+			snprintf(file_name, sizeof(argv[1]) + 3*sizeof(char), "%s.LZW", argv[1]);
+			output_file = fopen(file_name, "w");
+			break;
+		case DECODE:
+			printf("case is decode\n");
+			break;
+	}
 
 	return 0;
 }
