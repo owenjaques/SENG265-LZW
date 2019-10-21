54e6797354f69ee45d7d92ed5914c29fe6625780
Owen Jaques <owenjaques@uvic.ca>
2019-10-21 15:43:55 -0700

    got checking for wk in dictionary working

--- LZW.c
+++ LZW.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -16,6 +17,7 @@ unsigned char dict[DICTSIZE][ENTRYSIZE];  /* of 30 chars max; the first byte */
                                           /* will be reserved for padding    */
                                           /* the last byte (if necessary)    */
 
+//adds the initial 255 ASCII chars to dictionary
 void initDic(){
 	int i;
 	for(i = 0; i < 256; i++){
@@ -32,10 +34,37 @@ void initDic(){
 /* the 0 as the end of string marker). Again, writing the codes is handled   */
 /* by a separate function, just so I don't have to worry about writing 12    */
 /* bit numbers inside this algorithm.                                        */
-void encode(FILE *in, FILE *out) {
-	char w = 0;
+void encode(FILE *in, FILE *out){
+	char w[ENTRYSIZE] = {}; 
 	char k;
+	char wk[ENTRYSIZE];;
+	int w_length = 0;
 	for(k = fgetc(in); k != EOF; k = fgetc(in)){
+		int i;
+		//creates wk from w and k
+		for(i = 0; i < w_length; i++)
+			wk[i] = w[i];
+		wk[i] = k;
+		//sees if its in the dictionary
+		int in_dic = 0;
+		for(i = 0; i < DICTSIZE; i++){
+			int j;
+			for(j = 1; j <= w_length + 1; j++){
+				if(wk[j-1] == dict[i][j])
+					in_dic = 1;
+				else 
+					in_dic = 0;
+			}
+			if(in_dic)
+				break;
+		}
+		if(in_dic){
+			//sets w to wk
+			w_length++;
+			int i;
+			for(i = 0; i < w_length; i++)
+				w[i] = wk[i];
+		}
 	}
 }
 
@@ -46,6 +75,110 @@ void decode(FILE *in, FILE *out) {
 	// TODO implement
 }
 
+/*****************************************************************************/
+/* read12() handles the complexities of reading 12 bit numbers from a file.  */
+/* It is the simple counterpart of write12(). Like write12(), read12() uses  */
+/* static variables. The function reads two 12 bit numbers at a time, but    */
+/* only returns one of them. It stores the second in a static variable to be */
+/* returned the next time read12() is called.                                */
+int read12(FILE *infil)
+{
+ static int number1 = -1, number2 = -1;
+ unsigned char hi8, lo4hi4, lo8;
+ int retval;
+
+ if(number2 != -1)                        /* there is a stored number from   */
+    {                                     /* last call to read12() so just   */
+     retval = number2;                    /* return the number without doing */
+     number2 = -1;                        /* any reading                     */
+    }
+ else                                     /* if there is no number stored    */
+    {
+     if(fread(&hi8, 1, 1, infil) != 1)    /* read three bytes (2 12 bit nums)*/
+        return(-1);
+     if(fread(&lo4hi4, 1, 1, infil) != 1)
+        return(-1);
+     if(fread(&lo8, 1, 1, infil) != 1)
+        return(-1);
+
+     number1 = hi8 * 0x10;                /* move hi8 4 bits left            */
+     number1 = number1 + (lo4hi4 / 0x10); /* add hi 4 bits of middle byte    */
+
+     number2 = (lo4hi4 % 0x10) * 0x0100;  /* move lo 4 bits of middle byte   */
+                                          /* 8 bits to the left              */
+     number2 = number2 + lo8;             /* add lo byte                     */
+
+     retval = number1;
+    }
+
+ return(retval);
+}
+
+/*****************************************************************************/
+/* write12() handles the complexities of writing 12 bit numbers to file so I */
+/* don't have to mess up the LZW algorithm. It uses "static" variables. In a */
+/* C function, if a variable is declared static, it remembers its value from */
+/* one call to the next. You could use global variables to do the same thing */
+/* but it wouldn't be quite as clean. Here's how the function works: it has  */
+/* two static integers: number1 and number2 which are set to -1 if they do   */
+/* not contain a number waiting to be written. When the function is called   */
+/* with an integer to write, if there are no numbers already waiting to be   */
+/* written, it simply stores the number in number1 and returns. If there is  */
+/* a number waiting to be written, the function writes out the number that   */
+/* is waiting and the new number as two 12 bit numbers (3 bytes total).      */
+int write12(FILE *outfil, int int12)
+{
+ static int number1 = -1, number2 = -1;
+ unsigned char hi8, lo4hi4, lo8;
+ unsigned long bignum;
+
+ if(number1 == -1)                         /* no numbers waiting             */
+    {
+     number1 = int12;                      /* save the number for next time  */
+     return(0);                            /* actually wrote 0 bytes         */
+    }
+
+ if(int12 == -1)                           /* flush the last number and put  */
+    number2 = 0x0FFF;                      /* padding at end                 */
+ else
+    number2 = int12;
+
+ bignum = number1 * 0x1000;                /* move number1 12 bits left      */
+ bignum = bignum + number2;                /* put number2 in lower 12 bits   */
+
+ hi8 = (unsigned char) (bignum / 0x10000);                     /* bits 16-23 */
+ lo4hi4 = (unsigned char) ((bignum % 0x10000) / 0x0100);       /* bits  8-15 */
+ lo8 = (unsigned char) (bignum % 0x0100);                      /* bits  0-7  */
+
+ fwrite(&hi8, 1, 1, outfil);               /* write the bytes one at a time  */
+ fwrite(&lo4hi4, 1, 1, outfil);
+ fwrite(&lo8, 1, 1, outfil);
+
+ number1 = -1;                             /* no bytes waiting any more      */
+ number2 = -1;
+
+ return(3);                                /* wrote 3 bytes                  */
+}
+
+/** Write out the remaining partial codes */
+void flush12(FILE *outfil)
+{
+ write12(outfil, -1);                      /* -1 tells write12() to write    */
+}                                          /* the number in waiting          */
+
+/** Remove the ".LZW" extension from a filename */
+void strip_lzw_ext(char *fname)
+{
+    char *end = fname + strlen(fname);
+
+    while (end > fname && *end != '.' && *end != '\\' && *end != '/') {
+        --end;
+    }
+    if ((end > fname && *end == '.') &&
+        (*(end - 1) != '\\' && *(end - 1) != '/')) {
+        *end = '\0';
+    }
+}
 
 int main(int argc, char *argv[]){
 	if(argc <= 2 || argv[2][0] != 'e' && argv[2][0] != 'd'){
